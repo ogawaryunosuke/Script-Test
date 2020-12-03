1715f48a84431c163d82b9c1c279f9de96b83d2a
ogawaryunosuke <73462519+ogawaryunosuke@users.noreply.github.com>
2020-12-03 21:04:49 +0900

    プロジェクト作成

---
+++ スクリプトテスト２/Assets/Test2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int [] array = {1, 2, 3, 4, 5}
+
+        for (int i = 0; i < array.Length; i++)
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
