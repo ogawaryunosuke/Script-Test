46373c74785ef7cb4611b19b34fd57ef92aa4e9f
ogawaryunosuke <73462519+ogawaryunosuke@users.noreply.github.com>
2020-12-06 21:28:11 +0900

    script
    kadai

--- スクリプトテスト２/Assets/Test2.cs
+++ スクリプトテスト２/Assets/Test2.cs
@@ -7,9 +7,15 @@ public class Test2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int [] array = {1, 2, 3, 4, 5}
+        int [] array = {1, 2, 3, 4, 5};
 
         for (int i = 0; i < array.Length; i++)
+        {
+            if ( array[i] >= 5)
+            {
+                Debug.Log(array[i]);
+            }
+        }
     }
     
 
